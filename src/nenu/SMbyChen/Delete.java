e958b5abfe2cac4ea63211cb794b5c931467c0eb
unknown <1658942046@qq.com>
2019-04-26 17:10:04 +0800

    first'

---
+++ src/nenu/SMbyChen/Delete.java
@@ -0,0 +1,17 @@
+//package nenu.SMbyChen;
+//
+//import java.util.Scanner;
+//
+//public class Delete {
+//	private String name;
+//	
+//	public Delete() {
+//		Scanner scan=new Scanner(System.in);
+//		name=scan.nextLine();
+//	}
+//	
+//	public String getName() {
+//		return name;
+//	}
+//
+//}
